b8098afff19fc3257f49a3a515a2c8e286e125eb
alamindev07 <alamin.dev07@gmail.com>
2025-06-16 19:59:10 +0600

    assignment list created and displayed the all assignment page

--- src/pages/AssignmentList.jsx
+++ src/pages/AssignmentList.jsx
@@ -12,14 +12,21 @@ const AssignmentList = () => {
 
   return (
     <div className="max-w-5xl mx-auto mt-10 p-6 bg-white shadow rounded">
-      <h2 className="text-2xl font-bold mb-6 text-center">All Assignments</h2>
 
       {assignments.length === 0 ? (
-        <p className="text-center text-gray-500">No assignments created yet.</p>
+        <div className="flex justify-center items-center flex-col gap-4">        <p className="text-4xl text-center text-gray-500">No assignments created yet.</p>
+        <Link
+                  to="/create-assignment"
+                  className="btn btn-sm btn-primary w-fit"
+                >
+                  Create An Assignment
+                </Link>
+       
+</div>
       ) : (
         <div className="grid md:grid-cols-2 gap-6">
           {assignments.map((assignment) => (
-            <div key={assignment.id} className="card bg-base-100 shadow-md border p-4">
+            <div key={assignment.id} className="card bg-gray-100 shadow-md  p-4">
               <div className="card-body">
                 <h3 className="card-title text-lg font-semibold">{assignment.title}</h3>
                 <p className="text-sm text-gray-600 mb-1">
