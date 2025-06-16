3a7c27ccaac2c24fc0a52eeef13225cf0361a9cf
alamindev07 <alamin.dev07@gmail.com>
2025-06-16 15:55:57 +0600

    all basic setup done and all pakage installed

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCOtRvJMaGVGNbgPS6lVWmBk3XIT_SBHzA",
+  authDomain: "online-group-study-7bd5d.firebaseapp.com",
+  projectId: "online-group-study-7bd5d",
+  storageBucket: "online-group-study-7bd5d.firebasestorage.app",
+  messagingSenderId: "66235853758",
+  appId: "1:66235853758:web:d09402b34cac0f603f27ef"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+
+
---
+++ src/pages/MyAssignments.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MyAssignments = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default MyAssignments;
