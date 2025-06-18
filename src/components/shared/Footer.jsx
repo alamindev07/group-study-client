cddf748a1a01438d1b3c9c188453d6f1b0188a32
alamindev07 <alamin.dev07@gmail.com>
2025-06-18 23:25:53 +0600

    all code updated done

--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -1,18 +1,3 @@
-// const Footer = () => {
-//   return (
-//     <footer className="footer footer-center p-4 bg-base-200 text-base-content mt-10">
-//       <aside>
-//         <p>
-//           © {new Date().getFullYear()} GroupStudyHub. All rights reserved.
-//         </p>
-//       </aside>
-//     </footer>
-//   );
-// };
-
-// export default Footer;
-
-
 
 import { FaFacebook, FaTwitter, FaGithub, FaLinkedin } from "react-icons/fa";
 
